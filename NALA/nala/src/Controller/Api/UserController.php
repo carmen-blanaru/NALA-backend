9186edd6707938d0a7ac406d0871b753a1282d4d
Carmen Blanaru <blanaru.pro@gmail.com>
2021-07-19 18:56:45 +0200

    Two other paths created: add a new user and update a new user in Api\ UserController

--- NALA/nala/src/Controller/Api/UserController.php
+++ NALA/nala/src/Controller/Api/UserController.php
@@ -6,8 +6,12 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/api/v1/utilisateurs", name="api_user_", requirements={"id" = "\d+"})
@@ -34,6 +38,69 @@ class UserController extends AbstractController
             // the response is in json format so it is importent to serialize the objects in naming groups 
             'groups' => 'user',
         ]);
+    }
+
+    /**
+     * @Route("", name="add", methods={"POST"})
+     *
+     * Add a new user account to the database
+     */
+    public function newUserAccount(Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        // data in json format received from insomnia, javascript, etc
+        $jsonData = $request->getContent();
+
+        // it is necessary to tranform these datas in objects using the deserialize method
+        $user = $serializer->deserialize($jsonData, User::class, 'json');
+
+        // important step: verify if the user did errors during the creation of the account
+        $errors = $validator->validate($user);
+
+        // if everything is alright
+        if(count($errors) == 0) 
+        {   
+            // the datas are sent to the database and the process is stopped here with a 201 message
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->json([
+                'message' => 'L\'user ' . $user->getNickname() . ' a bien été ajouté'
+            ], 201);
+        }
+        // if there are any errors, a 500 message is displayed 
+        return $this->json([
+            'errors' => (string) $errors
+        ], 500);
+    }
+
+    /**
+     *  @Route("/{id}", name="update", methods={"PUT|PATCH"})
+     */
+    public function updateUserAccount(User $user, Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+         // data in json format received from insomnia, javascript, etc
+        $jsonData = $request->getContent();
+
+        // the serializer allows to transform the json format of the datas in objects froms the User class
+        // due to the dematerialize method
+        $user = $serializer->deserialize($jsonData, User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $user]);
+
+        $errors = $validator->validate($user);
+        if(count($errors) == 0)
+        {
+            $this->em->flush();
+
+            return $this->json([
+                'message' => 'Le compte a bien été mis à jour'
+            ]);
+        }
+
+        return $this->json([
+            'errors' => (string) $errors
+        ], 400);
+        // 400 => BAD request
+
+
     }
 
     /** 
