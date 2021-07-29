516c64cb846c70d9577c27f41128f07b0197befa
efosse <emmanuel.fosse@yahoo.co.uk>
2021-07-29 16:22:39 +0200

    first version of the post admin finished

--- NALA/nala/src/Controller/Admin/PostController.php
+++ NALA/nala/src/Controller/Admin/PostController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Post;
+use App\Form\Form\PostType;
 use App\Repository\PostRepository;
+use App\Service\PictureUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/admin/post", name="admin_post_")
+ */
 
 class PostController extends AbstractController
 {
     /**
-     * @Route("/admin/post", name="post")
+     * @Route("/", name="list")
      */
     public function index(PostRepository $postRepository): Response
     {
@@ -20,4 +27,56 @@ class PostController extends AbstractController
             'postList'        => $postList
         ]);
     }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     *
+     */
+    public function show(Post $post)
+    {
+        return $this->render('admin/post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+    /**
+     * @Route("/{id}/edition", name="edit", methods={"GET", "POST"})
+     *
+     */
+    public function editPost (Post $post, Request $request)
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form->isValid()) 
+        {
+   
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash('success', 'Le post a bien été mise à jour');
+
+            return $this->redirectToRoute('admin_post_list') ;
+        }
+        // if the post has not been submitted open the post edition form 
+       return $this->render('admin/post/edit.html.twig', [
+        'post' => $post,
+        'form' => $form->createView(),
+        ]); 
+    }
+
+    /**
+     * @Route("/{id}/delete", name="delete", methods={"GET","POST"})
+     *
+     */
+    public function deletePost (Post $post, Request $request)
+    {
+        // if ($this->isCsrfTokenValid('delete' . $post->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_post_list');
+        // }
+ 
+        
+    }    
+
 }
---
+++ NALA/nala/src/Form/Form/PostType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Title', null, [
+                'required' => true,
+                'label' => 'Titre'
+            ])
+            ->add('display', null, [
+                'label' => 'affichage'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
