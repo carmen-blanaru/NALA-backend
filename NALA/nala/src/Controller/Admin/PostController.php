82c1e413f734870d74255b62a46e366c5fc62b8e
efosse <emmanuel.fosse@yahoo.co.uk>
2021-07-29 12:30:31 +0200

    Main fields of the list of post added

--- NALA/nala/src/Controller/Admin/PostController.php
+++ NALA/nala/src/Controller/Admin/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class PostController extends AbstractController
     /**
      * @Route("/admin/post", name="post")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $postList = $postRepository->findAll();
         return $this->render('admin/post/index.html.twig', [
             'controller_name' => 'PostController',
+            'postList'        => $postList
         ]);
     }
 }
